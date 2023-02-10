827dc67fe32ce631864815785b278a148aa4cf6d
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-02-10 10:29:53 -0600

    finished

--- js/data-operators-variables.js
+++ js/data-operators-variables.js
@@ -1,25 +1,26 @@
 let a = 1;
 // a = 1
-let b = a++;
-// b = 1
-let c = ++a;
+let b = a++; //b = 1, a = 1
+// b = 1, a = 2
+let c = ++a; // a = 3
 // c = 3
+// a = 3, b = 1, c = 3
 let d = "hello";
-// hello +1
+// d = NaN
 let e = false;
-// false + 1
+// e = 1
 let preplexed;
 preplexed + 2;
-// undefined
+// NaN
 let price = 2.7;
 price.toFixed(2);
-// 2.7
+// "2.70"
 isNaN(0)
 // false
 isNaN(1)
 // false
 isNaN("")
-// true
+// false
 isNaN("string")
 //true
 isNaN("0")
@@ -66,14 +67,22 @@ isNaN(false)
 //true
 
 let sample = "Hello Codeup";
+//let sampleLength = sample.length;
 console.log(sample.length);
 //sample = sample.toLowerCase();
 console.log(sample);
 console.log(sample , "Students");
+// let sampleJoin = sample + "students";
+// let sampleJoinBetter = `${sample} Students`;
 console.log(sample , "Class");
+//let sampleJoinPlace = sampleJoin.replace( 'Students' , 'Class' );
 console.log(sample.indexOf('c'));
+//let cIndex = sampleJoinReplace.indexOf('c');
+//console.log(cIndex);
 console.log(sample.indexOf('C'));
+//let capCIndex = sampleJoinReplace.indexOf('C');
 let substring = sample.substring('6', '13');
+//let codeupWord = sampleJoinReplace.substring(capCIndex, 12);
 console.log( substring )
 
 let movie1 = "The little mermaid";
@@ -85,7 +94,7 @@ let cost2 = pricePerDay * 5 ;
 let cost3 = pricePerDay * 1 ;
 let totalCost = cost1 + cost2 + cost3
 console.log(totalCost);
-console.log( `Thank you for your rental of ${movie1}, ${movie2}, and ${movie3}. The total cost for renting these movies is $ ${totalCost.toFixed(2)}.`)
+console.log( `Thank you for your rental of ${movie1}, ${movie2}, and ${movie3}. \nThe total cost for renting these movies is $ ${totalCost.toFixed(2)}.`)
 
 let company1 = "Google";
 let company2 = "Amazon";
@@ -98,13 +107,55 @@ let hour2 = 4 ;
 let hour3 = 10;
 let weekPay = (rate1 * hour1) + (rate2 * hour2) + (rate3 * hour3);
 console.log(weekPay);
+//let formattedPaycheck = weekPay.toLocaleString("en-US, {style:"currency", currency:"USD"});
 console.log(`After working ${hour1} hours at ${company1}, ${hour2} hours at ${company2}, and ${hour3} hours at ${company3}. Your total payment is $${weekPay.toFixed(2)}`)
 
-let student = Boolean(false)
-let classSize = 100
-let classSchedule = 10
-if (student) {
-    classSize>=0 && classSchedule<=0
-}
-console.log(student)
+let classNotFull = true
+let classScheduleWorks = true
+let worksForStudent = classNotFull && classScheduleWorks
+console.log(worksForStudent)
+/* in class ex:
+let scheduleConflicts = false;
+let maxClassSize = 30;
+let currentClassSize = 24;
+let  canEnroll = !scheduleConflicts && currentClassSize < maxClassSize;
+ */
 
+let premium = true
+let offer = true
+let size = 4
+let haveDiscount = ( offer && (premium || size ))
+console.log(haveDiscount)
+/* in class ex:
+letMainCartSize = 2;
+let cartSize = 3;
+let premiumMember = false;
+let offerExpired = false;
+let canOfferApply = (premiumMember || (cartSize >= mainCartSize)) && !offerExpired
+ */
+
+
+let username = 'codeup';
+let password = 'notastrongpassword';
+
+let condition1 = password.length > 5;
+let condition2 = password.indexOf(username) === -1
+console.log(condition1)
+console.log(condition2)
+
+let condition3 = username.length <= 20;
+username.trim( );
+password.trim( );
+console.log(condition3)
+/* in class ex:
+let minChar = password.length >= 5;
+let noUserName = password.indexOf(username) >= 0;
+let maxUserChar = username.length <= 20;
+let userLength = username.length;
+let passLength = password.Length;
+let firstCharUser = username.substring(0, 1);
+let lastCharUser = username.substring(userLength -1), userLength);
+let firstCharPass = password.substring(0, 1);
+let lastCharPass = password.substring((passLength -1), passLength);
+let hasWhiteSpace = firstCharUser === ' ' || lastCharUser === ' ' || firstCharPass === ' ' || lastCharPass === ' ';
+ */
