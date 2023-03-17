6fbfb08f389a75d23fc44b462317a83e2dbdd099
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-03-17 14:00:00 -0500

    "completed selectors exercise"

---
+++ js/jquery-selectors-exercises.js
@@ -0,0 +1,29 @@
+jQuery("h3").click(function(){
+    $(this).next().slideToggle(500);
+});
+
+
+// $(document).ready(function(){
+//     alert("DOM has finished loading")
+// });
+//
+// let contentP1Tag = $('#p1').html();
+// alert(contentP1Tag);
+
+// $('.codeup').css("border", "1px solid red");
+
+$('li').css({
+   fontSize: 20,
+});
+
+$('h1, p, li').css({
+   backgroundColor: "ghostwhite",
+    border: '.5px solid grey',
+});
+
+let heading1 = $('h1').html();
+alert(heading1);
+
+
+
+
