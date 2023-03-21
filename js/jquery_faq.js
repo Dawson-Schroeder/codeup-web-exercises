7b7d1e46a9f922cb907910a3bef1bc7ce5cd3e29
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-03-21 11:35:01 -0500

    "completed jquery_faq exercise"

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,18 +2,40 @@
 let button = $('#button');
 let $viz = $('dd');
 let $highlight = $('dt');
-
-
     button.on('click', function(){
         $($viz).toggleClass('invisible');
     });
-
     $highlight.on('dblclick', function(){
            $(this).next().toggleClass('highlight')
     });
-
     $($highlight).on('click', function(){
        $(this).next().fadeToggle();
     });
+
+
+let $list = $('li');
+let $listButton = $('#yellow');
+    $($listButton).on('click', function(){
+      $($list).last().toggleClass('highlight');
+    });
+
+
+let $title = $('h3');
+    $($title).on('click', function(){
+        $(this).next().toggleClass('bold');
+    });
+    $($list).on('click', function(){
+        $(this).parent().find('li:first').toggleClass('blue');
+    });
+
+
+
+
+
+
+
+
+
+
 
 
